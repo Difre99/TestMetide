e4645cd522a2dbda561105c38fbad0decbdbc91e
Nicola De Bei <nicola.debei@metide.com>
2022-02-03 18:06:34 +0100

    funziona la JSONDecode quindi ho un array si CountryTemp (sono tutte stringhe)

--- TestMetide/ViewController.swift
+++ TestMetide/ViewController.swift
@@ -22,44 +22,43 @@ class ViewController: UIViewController {
      import FoundationNetworking
      #endif
 
-     struct Country{
+     struct CountryTemp: Decodable{
          let code2l: String
-         let code3l: String
-         let enabled: Bool
-         let flag: URL
-         let id: Int
-         let latitude: Float
-         let longitude: Float
          let name: String
-         let name_official: String
-         let zoom: Int
+         let flag: String
+         let latitude: String?
+         let longitude: String?
      }
+
+
      var semaphore = DispatchSemaphore (value: 0)
 
      var request = URLRequest(url: URL(string: "https://us-central1-job-interview-cfe5a.cloudfunctions.net/countries")!,timeoutInterval: Double.infinity)
      request.addValue("Basic ZGV2ZWxvcGVyOm1ldGlkZQ==", forHTTPHeaderField: "Authorization")
 
      request.httpMethod = "GET"
-
+     var data1 : Data
      let task = URLSession.shared.dataTask(with: request) { data, response, error in
          guard let data = data else {
              print(String(describing: error))
              semaphore.signal()
              return
          }
-         
-         //print(String(data: data, encoding: .utf8)!)
-         if let json = try? JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) {
-             //print((json as? Array<Any>)?[0])
+         semaphore.signal()
+         do{
+             let decoder: JSONDecoder = JSONDecoder.init()
+             let countryTemp: [CountryTemp] = try decoder.decode([CountryTemp].self, from: data)
+             for c in countryTemp{
+                 print(c.code2l)
+             }
              
+
+         }
+         catch let e{
+             print(e)
          }
          
-        
-         semaphore.signal()
      }
-
-
-
      task.resume()
      semaphore.wait()
 
